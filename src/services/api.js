edba7b40c23cda6a7fa57f92ddc4f0ada50400f9
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-09-19 00:05:55 -0300

    voltando a api anterior

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "http://api-salao-leila-production.up.railway.app",
+  baseURL: "https://api-salao-leila-production.up.railway.app",
 });
 
  api.interceptors.request.use(async (config) => {
