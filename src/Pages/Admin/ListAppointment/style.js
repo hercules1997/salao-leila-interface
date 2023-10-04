31a1dd3a0191cb0fe8e26245484bde7e51b61958
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-10-04 15:32:47 -0300

    removendo styles

--- src/Pages/Admin/ListAppointment/style.js
+++ src/Pages/Admin/ListAppointment/style.js
@@ -1,32 +1,20 @@
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
-import ModeEditIcon from '@mui/icons-material/ModeEdit'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import styled from 'styled-components'
+import CloseIcon from "@mui/icons-material/Close";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
   text-align: center;
   box-shadow: 0px 2px 5px gray;
-  .checkYes {
-    color: green;
-    border-radius: 28px;
-  }
-  .checkNo {
-    color: red;
-    border-radius: 28px;
-  }
 `;
-export const Img = styled.img`
-  width: 70px;
-  height: 70px;
-  border-radius: 8px;
-`
-export const DeleteForeverIconStyle = styled(DeleteForeverIcon)`
+
+export const DeleteForeverIconStyle = styled(CloseIcon)`
   width: 80px;
   color: red;
   cursor: pointer;
@@ -34,7 +22,21 @@ export const DeleteForeverIconStyle = styled(DeleteForeverIcon)`
     font-weight: bold;
     color: rosybrown;
   }
-`
+`;
+
+export const ListVoid = styled.div`
+  display: flex;
+  width: 100%;
+  height: 500px;
+  font-size: 4rem;
+  justify-content: center;
+  align-items: center;
+  box-shadow: none;
+  background-color: #fff;
+  border-radius: 8px;
+  color: #9e775a;
+`;
+
 export const ModeEditIconStyle = styled(ModeEditIcon)`
   width: 80px;
   color: gray;
@@ -43,23 +45,22 @@ export const ModeEditIconStyle = styled(ModeEditIcon)`
     font-weight: bold;
     color: #000;
   }
-`
+`;
 export const TableRowStyle = styled(TableRow)`
   font-weight: bold;
   height: 50px;
   background: #ffff !important;
-
 `;
 export const TableCellStyle = styled(TableCell)`
   width: 20px;
   color: #000 !important;
   font-size: 1.2rem !important;
-`
+`;
 
-export const TableHeadStyle = styled(TableHead)``
+export const TableHeadStyle = styled(TableHead)``;
 export const TableContainerStyle = styled(TableContainer)`
   border: none !important;
   border-radius: 12px !important;
-`
-export const TableBodyStyle = styled(TableBody)``
-export const TableStyle = styled(Table)``
+`;
+export const TableBodyStyle = styled(TableBody)``;
+export const TableStyle = styled(Table)``;
